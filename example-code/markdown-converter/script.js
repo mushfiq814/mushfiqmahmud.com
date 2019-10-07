e5f0e83486eb336ef7fa9c7393ef361be30f301b
mushfiq814 <mushfiq@mail.usf.edu>
2019-10-07 00:19:00 -0400

    Updated homepage and resume

---
+++ example-code/markdown-converter/script.js
@@ -0,0 +1,115 @@
+let converter = new showdown.Converter();
+
+let md1 = `
+
+# PROFILE
+
+My name is Mushfiq Mahmud. I studied the theory and practices of engineering while completing my Bachelors of Science in Electrical Engineering at the Univeristy of South Florida in Tampa, Florida.
+
+[Download PDF](/assets/docs/Resume.pdf)
+
+# PROFESSIONAL EXPERIENCE
+
+## [Disciplined Minds Tutoring, LLC](disciplinedmindstutoring.com)
+### Sys Admin, Web Developer, Tutor
+### Tampa, FL | December 2017 to present
+
+* Developed entire application system to automate day-to-day admin tasks and reduce around 20 man hours per week. This application was created in Apple FileMaker with an integrated back-end with thousands of records and had a complete customer ready front-end UI that is used by around 35-50 people daily. The app has scripts for multiple functions including sending emails to customers, grading Standardized Testing results, employee sign-in and timesheet calculation for payroll, etc.
+* Wrote a server Middleware API in PHP that connected our FileMaker application to other APIs such as Google Calendar API, Google Mail API, Authorize.net Payment API and FileMaker’s Data API.
+* Developed and maintained website using PHP, HTML, JavaScript and MySQL that dynamically displays detailed company information in a simple way for new customer retention. It also includes online booking capabilities with direct employee Google calendar integration and smart availability checking to avoid appointment conflicts.
+* Written scripts in JavaScript and NodeJS to automate data transfer between appointments calendars and POS system using Google Calendar API, Google App Script and Square POS API which helped speed up daily admin process and saved work hours.
+* Overseen entire system transition process which affected appointment bookings and transaction management.
+* Created and maintained an active Dashboard using Google App Scripting (JavaScript) that enables the admin staff to perform crucial daily duties including transaction management, flagging special transactions and measuring complete employee hours.
+* Built a custom messaging system between tutors and students using Google’s Gmail Service and spreadsheets as the back-end while the front-end doubled as a system for logging daily student attendance with digital signatures.
+* Developed the website with structural updates such as setting up an appointment system as well as regular content updates.
+* Developed and maintained WordPress website using PHP, HTML, JavaScript and MySQL that dynamically displays detailed company information in a simple way for new customer retention with online booking capabilities.
+* Written scripts to automate data transfer between appointments calendars and POS system using Google Calendar API, Google App Script and Square POS API which helped speed up daily admin process and saved work hours.
+* Overseen entire system transition process which affected appointment bookings and transaction management.
+
+
+## [USF Holcombe Jenkins Honors Program](http://honors.usf.edu/tutors.html)
+### Tutor
+### Tampa, FL | August 2016 to June 2017
+
+* Tutor for EE: Circuits, Semiconductor Devices, Electronics.
+
+## [USF SMART LAB USF Tampa Library](http://honors.usf.edu/tutors.html)
+### Tutor
+### Tampa, FL | August 2015 to June 2017
+
+* Tutor for Calculus (I-III) & Physics (I, II).
+* Helped in improving the grades of over 500 students. Received very good rating overall.
+* CRLA Tutor Training Certification Level 1 & 2 including collaboration with professors & tutors on learning strategies.
+
+# EDUCATION
+
+## [University of South Florida](http://www.usf.edu/engineering/ee/)
+
+### Bachelors of Science in Electrical Engineering, Spring 2017...
+### Graduated from the Honors College
+
+Papers:
+* System on Chip - [IIPS Infrastructure IP for Secure SoC Design](/assets/docs/Paper_SoC_IIPS.pdf)
+* Communication Systems - [BPCS Steganography](/assets/docs/Paper_Comms_BPCS_Steganography.pdf)
+* Electromagnetics - [How Electric Guitar Pickups Work](/assets/docs/Paper_EM_Magnetic_Pickups.pdf)
+* Honors Geo Perspectives - Tiananmen Square Massacre and Modern China: [Slides](/assets/docs/Paper_Honors_China_Slides.pdf) and [Paper](/assets/docs/Paper_Honors_China_Paper.pdf).
+
+Courses completed:
+* Embedded Systems
+* System on a Chip
+* Microprocessor Architecture
+* Signal Analysis
+* Microelectronics
+* Communication Systems
+
+# PROJECTS
+
+For detailed information check out the projects page.
+
+## [Senior Design: RoboPainter Team Leader (Held 3rd Place out of 8 teams)](assets/project_files/project_1_RoboPainter/project1_robopainter.html)
+* Automated microcontroller interior wall painting robot prototype – in charge of programming & 3D printing & CAD design.
+* Made use of C/C++, interrupts & timing programming, DC motor control circuits, solenoid spray valves, power supply design
+* Successfully completed prototype with testing and design requirements met.
+
+## [Stove Safety Monitor System](assets/project_files/project_4_Stove_Safety/project4_stove.html)
+* Designed Timing circuit controlled by laser & temperature sensors built using discrete logic.
+* Made use of assembly code, Embedded C++, ADC, interrupts, USB, I2C Master & Slave communication & LCD output.
+
+## [Digital Chord Chart](assets/project_files/project_2_Digital_Chord_Chart/project2_dcc.html)
+* 3D printed remote controlled guitar fretboard model that can teach guitar chords & scales using LED matrix & LCD Panel.
+* Made use of IR remote and receiver, LED Matrix pattern generation & interrupt service routines for sound generation.
+
+## [Honors Engineering Thesis: Engineering Sustainability for On-Campus Food Waste](assets/project_files/project_6_Honors_App/project6_honor.html)
+* Team of 4 designed mobile application to record survey data for dining halls to improve communication & reduce food waste.
+* Used MongoDB (JSON based) for database management, Angular & Ionic for web app development.
+
+## [Personal Website](index.html)
+* Learned web development for the purpose of self promotion.
+
+## Guitar Sound Effects Pedal
+* Guitar overdrive + distortion music footswitch that alters the sound of a real guitar using DSP techniques.
+
+## [Atari Punk Console + Decade Sequencer](assets/project_files/project_3_Atari_Punk_Console/project3_apc.html)
+* Monostable 555 coupled with adjustable 10 step sequencer for producing music.
+
+# STUDENT ORGANIZATIONS
+
+## IEEE-Computer Society USF Chapter
+### March 2016 to November 2016
+* Held position of Historian & Media Outreach Officer maintaining online presence & team collaboration.
+
+## American Water Works Association USF Chapter
+### January 2016 to May 2016
+* Held position of Treasurer & helped with Event management & Budget Handling.
+
+# AND WHEN I'M NOT WORKING, I LIKE TO...
+* Play Guitar.
+* Learn things on YouTube.
+* Learn Design/Theme my Android.
+
+`;
+
+
+let html1 = converter.makeHtml(md1);
+document.getElementById('result').innerHTML = html1;
+
